a4452505cfeed35b94a39defe4dd77500e98407d
Markov Alex <markovalex95@gmail.com>
2024-09-28 20:00:13 +0300

    Change parsers interface
    Change parser interface to return the parser function instead of parsing
    immediately in-place. That's how actually parser combinators should
    work. We need all parsers to have same signature so they can be combined
    by higher-order combinators like and(), or().
    
    Before:
    fn foo(&str, /* params */ ) -> ParserResult<T>
    
    After:
    fn foo(/*params*/) -> impl FnOnce(&str) -> ParserResult<T>

--- src/combinators.rs
+++ src/combinators.rs
@@ -16,82 +16,96 @@ impl<'a, T> Parsed<'a, T> {
     }
 }
 
+/// Helper function to make Parsed<> construction less verbose
+fn parsed<'a, T>(value: T, residual: &'a str) -> Parsed<'a, T> {
+    Parsed { value, residual }
+}
+
 /// Result of the parsing
 /// Some - parsing successful
 /// None - parsed failed
 pub type ParserResult<'a, T> = Option<Parsed<'a, T>>;
 
 
-/// Parses fixed string
-pub fn tag<'a, 'b>(str: &'a str, tag: &'b str) -> ParserResult<'a, &'a str> {
-    if str.starts_with(tag) {
-        Some(Parsed::new(&str[..tag.len()], &str[tag.len()..]))
-    } else {
-        None
-    }
-}
-
-
-/// Parses two subsequent expressions
-pub fn and<'a, F1, F2, R1, R2>(f1: F1, f2: F2, str: &'a str) -> ParserResult<'a, (R1, R2)>
-    where F1: FnOnce(&'a str) -> ParserResult<'a, R1>,
-          F2: FnOnce(&'a str) -> ParserResult<'a, R2>
-{
-    if let Some(res1) = f1(&str) {
-        if let Some(res2) = f2(res1.residual) {
-            Some(Parsed::new((res1.value, res2.value), &res2.residual))
+/// Creates a parsed which expects a given string
+pub fn tag<'a, 'b>(tag: &'b str) -> impl FnOnce(&'a str) -> ParserResult<&'a str> + 'b {
+    move |str: &'a str| {
+        if str.starts_with(tag) {
+            Some(parsed(&str[..tag.len()], &str[tag.len()..]))
         } else {
             None
         }
-    } else {
-        None
     }
 }
 
 
-/// Parses either first expression or second
-pub fn or<'a, F1, F2, R>(f1: F1, f2: F2, str: &'a str) -> ParserResult<'a, R>
+/// Combines two parsers to parse both subsequent expressions
+pub fn and<'a, F1, F2, R1, R2>(f1: F1, f2: F2) -> impl FnOnce(&'a str) -> ParserResult<'a, (R1, R2)>
+    where F1: FnOnce(&'a str) -> ParserResult<'a, R1>,
+          F2: FnOnce(&'a str) -> ParserResult<'a, R2>
+{
+    |str: &'a str| {
+        if let Some(res1) = f1(&str) {
+            if let Some(res2) = f2(res1.residual) {
+                Some(Parsed::new((res1.value, res2.value), &res2.residual))
+            } else {
+                None
+            }
+        } else {
+            None
+        }
+    }
+}
+
+
+/// Combines two parsers to parse either first expression or another
+pub fn or<'a, F1, F2, R>(f1: F1, f2: F2) -> impl FnOnce(&'a str) -> ParserResult<'a, R>
     where F1: FnOnce(&'a str) -> ParserResult<'a, R>,
           F2: FnOnce(&'a str) -> ParserResult<'a, R>
 {
-    let res1 = f1(str);
-    if res1.is_some() {
-        res1
-    } else {
-        f2(str)
-    }
-}
-
-/// Consumes a whitespace
-pub fn whitespace(str: &str) -> ParserResult<()> {
-    for (i, c) in str.chars().enumerate() {
-        if !char::is_whitespace(c) {
-            return Some(Parsed::new((), &str[i..]));
-        }
-    }
-
-    Some(Parsed::new((), &str[str.len()..]))
-}
-
-
-/// Parses number
-pub fn number(str: &str) -> ParserResult<u32> {
-    let mut value: u32 = 0;
-    let mut i = 0;
-
-    for c in str.chars() {
-        if let Some(digit) = c.to_digit(10) {
-            value = value.checked_mul(10)?.checked_add(digit)?;
-            i +=  1;
+    |str: &'a str| {
+        let res1 = f1(str);
+        if res1.is_some() {
+            res1
         } else {
-            break;
+            f2(str)
         }
     }
+}
 
-    if i > 0 {
-        Some(Parsed::new(value, &str[i..]))
-    } else {
-        None
+/// Creates a parser to skip a whitespace
+pub fn whitespace() -> impl FnOnce(&str) -> ParserResult<()> {
+    |str| {
+        for (i, c) in str.chars().enumerate() {
+            if !char::is_whitespace(c) {
+                return Some(Parsed::new((), &str[i..]));
+            }
+        }
+        Some(Parsed::new((), &str[str.len()..]))
+    }
+}
+
+
+/// Creates parser to parse an u32 integer
+pub fn int_u32() -> impl FnOnce(&str) -> ParserResult<u32> {
+    |str| {
+        let mut value: u32 = 0;
+        let mut i = 0;
+
+        for c in str.chars() {
+            if let Some(digit) = c.to_digit(10) {
+                value = value.checked_mul(10)?.checked_add(digit)?;
+                i +=  1;
+            } else {
+                break;
+            }
+        }
+
+        if i > 0 {
+            Some(Parsed::new(value, &str[i..]))
+        } else {
+            None
+        }
     }
 }
 
@@ -102,72 +116,73 @@ mod tests {
 
     #[test]
     fn test_tag() {
-        let res = tag("abc123", "abc");
+        let res = tag("abc")("abc123");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, "abc");
         assert_eq!(res.unwrap().residual, "123");
 
-        let res = tag("abc", "abc");
+        let res = tag("abc")("abc");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, "abc");
         assert_eq!(res.unwrap().residual, "");
 
-        let res = tag("123", "abc");
+        let res = tag("abc")("123");
         assert!(res.is_none());
     }
 
     #[test]
     fn test_and() {
-        let parser = |str| and(|x| tag(x, "abc"), |x| tag(x, "def"), str);
-
-        let res = parser("abcdef123");
+        let res = and(tag("abc"), tag("def"))("abcdef123");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, ("abc", "def"));
         assert_eq!(res.unwrap().residual, "123");
 
-        let res = parser("def123");
+        let res = and(tag("abc"), tag("def"))("abcdef");
+        assert!(res.is_some());
+        assert_eq!(res.unwrap().value, ("abc", "def"));
+        assert_eq!(res.unwrap().residual, "");
+
+        let res = and(tag("abc"), tag("def"))("def");
         assert!(res.is_none());
 
-        let res = parser("abc123");
+        let res = and(tag("abc"), tag("def"))("def");
         assert!(res.is_none());
     }
 
     #[test]
     fn test_or() {
-        let parser = |str| or(|x| tag(x, "abc"), |x| tag(x, "def"), str);
-
-        let res = parser("abcdef123");
+        let res = or(tag("abc"), tag("def"))("abcdef123");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, "abc");
         assert_eq!(res.unwrap().residual, "def123");
 
-        let res = parser("abc123");
+        let res = or(tag("abc"), tag("def"))("abc123");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, "abc");
         assert_eq!(res.unwrap().residual, "123");
 
-        let res = parser("def123");
+        let res = or(tag("abc"), tag("def"))("def123");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, "def");
         assert_eq!(res.unwrap().residual, "123");
 
-        let res = parser("123");
+        let res = or(tag("abc"), tag("def"))("123");
         assert!(res.is_none());
     }
 
     #[test]
     fn test_whitespace() {
-        let res = whitespace("hello");
+        let res = whitespace()("hello");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, ());
         assert_eq!(res.unwrap().residual, "hello");
 
-        let res = whitespace("    hello");
+        let res = whitespace()("    hello");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, ());
         assert_eq!(res.unwrap().residual, "hello");
 
-        let res = whitespace("    ");
+        let res = whitespace()("    ");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, ());
         assert_eq!(res.unwrap().residual, "");
@@ -175,17 +190,17 @@ mod tests {
 
     #[test]
     fn test_number() {
-        let res = number("1234");
+        let res = int_u32()("1234");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, 1234);
         assert_eq!(res.unwrap().residual, "");
 
-        let res = number("1234hello");
+        let res = int_u32()("1234hello");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, 1234);
         assert_eq!(res.unwrap().residual, "hello");
 
-        let res = number("hello");
+        let res = int_u32()("hello");
         assert!(res.is_none());
     }
 }
--- src/expression.rs
+++ src/expression.rs
@@ -12,22 +12,26 @@ pub enum Operand {
 }
 
 
-/// Parses "+" or "-"
-pub fn plus_minus(str: &str) -> ParserResult<Operand> {
-    match str.chars().next()? {
-        '+' => Some(Parsed::new(Operand::PLUS, &str[1..])),
-        '-' => Some(Parsed::new(Operand::MINUS, &str[1..])),
-        _ => None
+/// Creates a parser to parse "+" or "-"
+pub fn plus_minus() -> impl FnOnce(&str) -> ParserResult<Operand> {
+    |str| {
+        match str.chars().next()? {
+            '+' => Some(Parsed::new(Operand::PLUS, &str[1..])),
+            '-' => Some(Parsed::new(Operand::MINUS, &str[1..])),
+            _ => None
+        }
     }
 }
 
 
-// Parses "*" or "/"
-pub fn mul_div(str: &str) -> ParserResult<Operand> {
-    match str.chars().next()? {
-        '*' => Some(Parsed::new(Operand::MULT, &str[1..])),
-        '/' => Some(Parsed::new(Operand::DIV, &str[1..])),
-        _ => None
+// Creates a parser to parses "*" or "/"
+pub fn mul_div() -> impl FnOnce(&str) -> ParserResult<Operand> {
+    |str| {
+        match str.chars().next()? {
+            '*' => Some(Parsed::new(Operand::MULT, &str[1..])),
+            '/' => Some(Parsed::new(Operand::DIV, &str[1..])),
+            _ => None
+        }
     }
 }
 
@@ -38,33 +42,33 @@ mod tests {
 
     #[test]
     fn test_plus_minus() {
-        let res = plus_minus("+ 1");
+        let res = plus_minus()("+ 1");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, Operand::PLUS);
         assert_eq!(res.unwrap().residual, " 1");
 
-        let res = plus_minus("- 1");
+        let res = plus_minus()("- 1");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, Operand::MINUS);
         assert_eq!(res.unwrap().residual, " 1");
 
-        let res = plus_minus("1 + 1");
+        let res = plus_minus()("1 + 1");
         assert!(res.is_none());
     }
 
     #[test]
     fn test_mul_div() {
-        let res = mul_div("* 1");
+        let res = mul_div()("* 1");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, Operand::MULT);
         assert_eq!(res.unwrap().residual, " 1");
 
-        let res = mul_div("/ 1");
+        let res = mul_div()("/ 1");
         assert!(res.is_some());
         assert_eq!(res.unwrap().value, Operand::DIV);
         assert_eq!(res.unwrap().residual, " 1");
 
-        let res = mul_div("1 * 1");
+        let res = mul_div()("1 * 1");
         assert!(res.is_none());
     }
 }
